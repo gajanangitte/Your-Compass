4eb4d31b1afdd4f33c8e43ad1a341cc9cc8c6975
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-05-28 21:29:29 +0530

    Completed Feed Me

---
+++ src/utils/Yelp.js
@@ -0,0 +1,52 @@
+const apiKey = "3x6hCt6LcuCGAsLG1CUZwR3FF6LwyPfA_O_5VMxSUcy-2J1Pw9Y4d8kkG1v1lNZ55UKpMez2JZOWhmqzPALtBvtuerAe-v6aa_D8Emh86y1-fNKan366_sWvFMTPXnYx";
+
+const Yelp = {
+ search(term, location, sortBy) {
+
+	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+		headers : {
+			Authorization: `Bearer ${apiKey}`
+		}
+	} ).then( (response) => {
+		return response.json();
+	}).then( (jsonResponse) => {
+		if(jsonResponse.businesses) {
+			return jsonResponse.businesses.map( (business) => {
+				return {
+					id: business.id,
+					imageSrc: business.image_url,
+					name: business.name,
+					address: business.location.address1,
+					city: business.location.city,
+					state: business.location.state,
+					zipCode: business.location.zip_code,
+					category: business.categories[0].title,
+					rating: business.rating,
+					reviewCount: business.review_count,
+				}
+			})
+
+		} else {
+			  
+			  return {
+			  id: 'Not Available',
+			  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+			  name: 'Not Available',
+			  address: 'Not Available',
+			  city: 'Not Available',
+			  state: 'Not Available',
+			  zipCode: 'Not Available',
+			  category: 'Not Available',
+			  rating: 0,
+			  reviewCount: 0
+			  }
+		}
+
+
+	} );
+
+	}
+
+};
+
+export default Yelp;
