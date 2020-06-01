60ba787cf081259218a453dbc46573110515b38d
gajanangitte <gajanan_s@cs.iitr.ac.in>
2020-06-01 12:21:09 +0530

    Fixed vulnerabilities

--- src/utils/Yelp.js
+++ src/utils/Yelp.js
@@ -1,4 +1,4 @@
-const apiKey = "3x6hCt6LcuCGAsLG1CUZwR3FF6LwyPfA_O_5VMxSUcy-2J1Pw9Y4d8kkG1v1lNZ55UKpMez2JZOWhmqzPALtBvtuerAe-v6aa_D8Emh86y1-fNKan366_sWvFMTPXnYx";
+const apiKey = "";
 
 const Yelp = {
  search(term, location, sortBy) {
